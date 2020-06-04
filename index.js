8a2c629f19681b79f872a6db965f3c1b0137f73d
Arnaud Huc <arnaud.huc.pro@gmail.com>
2020-06-04 21:26:20 +0200

    Move onMessage event into is own file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const { Client, Collection } = require('discord.js')
 const { prefix, token, messageId, availableRoles } = require('./config.json')
+const onMessage = require('./events/onMessage')
+const onMessageReactionAdd = require('./events/onMessageReactionAdd')
+const onMessageReactionRemove = require('./events/onMessageReactionRemove')
 
 const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 client.commands = new Collection()
@@ -21,77 +24,7 @@ client.once('ready', () => {
 })
 
 client.on('message', (message) => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return
-
-	const args = message.content.slice(prefix.length).split(/ +/)
-	const commandName = args.shift().toLowerCase()
-
-	const command =
-		client.commands.get(commandName) ||
-		client.commands.find(
-			(cmd) => cmd.aliases && cmd.aliases.includes(commandName),
-		)
-
-	if (!command) return
-
-	if (command.guildOnly && message.channel.type !== 'text') {
-		return message.reply("I can't execute that command inside DMs!")
-	}
-
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``
-		}
-
-		return message.channel.send(reply)
-	}
-
-	if (command.permission) {
-		if (
-			!message.member.hasPermission([
-				'KICK_MEMBERS',
-				'BAN_MEMBERS',
-				'MANAGE_MESSAGES',
-			])
-		) {
-			return message.channel.send(
-				"Tu n'as pas les droits suffisant pour effectuer cette commande",
-			)
-		}
-	}
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Collection())
-	}
-
-	const now = Date.now()
-	const timestamps = cooldowns.get(command.name)
-	const cooldownAmount = (command.cooldown || 3) * 1000
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000
-			return message.reply(
-				`please wait ${timeLeft.toFixed(
-					1,
-				)} more second(s) before reusing the \`${command.name}\` command.`,
-			)
-		}
-	}
-
-	timestamps.set(message.author.id, now)
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
-
-	try {
-		command.execute(message, args)
-	} catch (error) {
-		console.error(error)
-		message.reply('there was an error trying to execute that command!')
-	}
+	onMessage.execute(message, prefix, client, cooldowns)
 })
 
 client.on('messageReactionAdd', async (reaction, user) => {
