52b00b6fea2cd05f0e8162d763139616c5a6b43a
Huc Arnaud <arnaud.huc@adevinta.com>
2022-10-31 15:45:06 +0100

    feat(custom): add roll

--- commands/roll.ts
+++ commands/roll.ts
@@ -4,17 +4,21 @@ export default customCommand(
   async (interaction, option) => {
     if (!option) return
 
-    const [number, max] = option.description.split('d')
+    const [number, rest] = option.description.split('d')
+    const [max = '1', modifier = '0'] = rest.split('+')
     let results: number[] = []
     let total: number = 0
+    console.log(max, number, modifier)
 
     for (let index = 0; index < parseInt(number); index++) {
-      const roll = Math.floor(Math.random() * parseInt(max))
+      const roll = Math.floor(Math.random() * (parseInt(max) - 1 + 1) + 1)
       results.push(roll)
       total = total + roll
     }
 
-    return `Rolled ${total} : [${results}]`
+    total = total + parseInt(modifier)
+
+    return `Rolled ${total} : [${results}] + ${modifier}`
   },
   {
     name: 'roll',
